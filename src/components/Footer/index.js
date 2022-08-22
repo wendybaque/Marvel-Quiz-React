724419e36de4e7f9b6a33345b99089bb6d87c14d
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-22 14:25:51 +0200

    Vidéo 119 : Adding buttons and animations in Landing component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
         <div className='footer-container'>
-            <p>Projet réalisé par Donkey Geek | 2020 | Udemy</p>
+            <p>Projet réalisé par Wendy Baqué, d'après Donkey Geek | 2020-2022 | Udemy</p>
             <p>Les icônes proviennent de <a href="https://www.iconfinder.com/search?q=marvel" alt="redirection vers Iconfinder.com" aria-label='redirection vers Iconfinder.com'>Iconfinder.com</a></p>
         </div>
     </footer>
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useRef, useEffect, useState, Fragment} from 'react'
 
 const Landing = () => {
-  return (
-    <main className='welcomePage'>
 
+  // Via useRef : dit que la div rouge est l'afficahge de base/de référence.
+  const refWolverine = useRef(null);
+
+  // Affiche les griffes et les retire au bout de 3s.
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+    setTimeout(() => {
+      refWolverine.current.classList.remove("startingImg");
+      setBtn(true)
+    }, 1500);
+  }, [])
+
+  // Gère l'afficahge des boutons d et g.
+  const [btn, setBtn] = useState(false);
+
+  // Gère les conditions de l'afficahge des boutons.
+  const displayBtn = btn && (
+    <Fragment>
+      <div className='leftBox'>
+        <button className='btn-welcome'>Inscription</button>
+      </div>
+      <div className='rightBox'>
+        <button className='btn-welcome'>Connexion</button>
+      </div>
+    </Fragment>
+  )
+
+  return (
+    <main ref={refWolverine} className='welcomePage'>
+      {displayBtn}
     </main>
   )
 }
