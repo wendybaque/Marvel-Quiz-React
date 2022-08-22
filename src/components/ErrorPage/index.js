b7bb6b9ad708eb6a0e485c60585659e335d3dab7
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-22 15:42:06 +0200

    Vidéo 123 : stylize Error Page component with inline style

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import batman from '../../assets/batman.png';
+
+const centerH2 = {
+  textAlign: 'center',
+  marginTop: '50px'
+}
+
+const centerIMG = {
+  display: 'block',
+  margin:'40px auto'
+}
 
 const ErrorPage = () => {
   return (
     <div className='quiz-bg'>
       <div className='container'>
-        Error Page
+        <h2 style={centerH2}>Oups, cette page n'existe pas...</h2>
+        <img style={centerIMG} src={batman} alt="Logo de Batman"/>
       </div>
     </div>
   )
