4e4f88426eba342a326c2c26d783a13839c20193
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-23 16:55:49 +0200

    Gestion de la session utilisateur vidéo 140

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState, Fragment, useContext, useEffect} from 'react';
+import {useNavigate } from 'react-router-dom'
+
+import {FirebaseContext} from '../Firebase';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
 
-const Welcome = () => {
-  return (
+const Welcome = (props) => {
+
+  const navigate = useNavigate();
+
+  const firebase = useContext(FirebaseContext);
+
+  const [userSession, setUserSession] = useState(null);
+
+  // Accès à Firebase -> vérifie si l'utilisateur est connecté.
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged(user => {
+      user ? setUserSession(user) : navigate('/')
+    })
+  
+    return () => {
+      listener()
+    }
+  }, [])
+  
+
+  return userSession === null ? (
+    <Fragment>
+      <div className='loader'></div>
+      <p>Loading...</p>
+    </Fragment>
+  ) : (
     <div className='quiz-bg'>
         <div className='container'>
             <Logout />
