81c03b863897c6660d42348d6542c38095a0221f
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-24 12:10:17 +0200

    Vidéos 149-150 : récupérer les questions à poser dans le quiz

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Levels from '../Levels';
 import ProgressBar from '../ProgressBar';
+import { QuizMarvel } from '../QuizMarvel';
 
+class Quiz extends Component {
 
-const Quiz = (props) => {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizLevel:0,
+    maxQuestions:10,
+    storedQuestions:[],
+    question: null,
+    options:[],
+    idQuestion: 0
+  }
+
+  loadQuestions = quizz => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+    if(fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchedArrayQuiz.map(({answer, ...keepRest}) => keepRest);
+      this.setState({
+        storedQuestions:newArray
+      })
+    } else {
+      console.log("Pas assez de questions...");
+    }
+  }
+
+  componentDidMount(){
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(provProps, prevState) {
+    if(this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options
+      })
+    } else {
+
+    }
+  }
+
+  render() {
+
+    const {pseudo} = this.props.userData;
+
+    const displayOptions = this.state.options.map((option, index) => {
+      return(
+        <p key={index} className='answerOptions'>{option}</p>
+      )
+    })
+
   return (
     <div>
-      <h2>Pseudo : {props.userData.pseudo}</h2>
+      <h2>Pseudo : {pseudo}</h2>
       <Levels />
       <ProgressBar />
-      <h2>Notre question Quiz</h2>
-      <p className='answerOptions'>Question 1</p>
-      <p className='answerOptions'>Question 2</p>
-      <p className='answerOptions'>Question 3</p>
-      <p className='answerOptions'>Question 4</p>
+      <h2>{this.state.question}</h2>
+        {displayOptions}
       <button className='btnSubmit'>Suivant</button>
     </div>
   )
+  }
 }
 
 export default Quiz;
