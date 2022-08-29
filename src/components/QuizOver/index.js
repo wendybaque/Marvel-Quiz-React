fc7b8cca4f6b2d975dba0463003aa5ca56f3af97
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-08-29 10:53:41 +0200

    Vidéos 165-166-167 : Axios & Marvel API and modal component with data

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -8,13 +8,14 @@ import { BsFillHandThumbsUpFill } from 'react-icons/bs';
 import Loader from '../Loader';
 import Modal from '../Modal';
 
+import axios from 'axios';
+
 const QuizOver = React.forwardRef((props, ref) => {
 
   const {levelNames, score, maxQuestions, quizLevel, percent, loadLevelQuestions} = props; 
   
   // Récupération de la clé API :
-  const API_PUBLIC_KEY = process.env.REACT_APP_MARVEL_API_KEY
-  console.log(API_PUBLIC_KEY)
+  const API_PUBLIC_KEY = process.env.REACT_APP_MARVEL_API_KEY;
   // Récupération du hash :
   const hash = '2db2ee218c7dd2c6034acd250726a3f4';
 
@@ -22,19 +23,63 @@ const QuizOver = React.forwardRef((props, ref) => {
   const [asked, setAsked] = useState([]);
   // State du popup :
   const [openModal, setOpenModal] = useState(false);
+  // State des infos sur les characters dans le popup :
+  const [characterInfo, setCharacterInfo] = useState([]);
+  // State de gestion du loading des résultats de l'API :
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setAsked(ref.current)
+
+    if(localStorage.getItem('marvelStorageDate')) {
+      const date = localStorage.getItem('marvelStorageDate');
+      checkDataAge(date);
+    }
   }, [ref])
 
-  // Affichage du popup :
+  // Fonction de comparaison de date de fetch de l'API et de la date actuelle :
+  const checkDataAge = (date) => {
+    // Conversion des différentes dates :
+    const today = Date.now();
+    const timeDifference = today - date;
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+  
+    // Création de nouvelle data si le dernier fetch dans le localstorage date de + de 15 jours
+    if(daysDifference >= 15) {
+      localStorage.clear();
+      localStorage.setItem('marvelStorageDate', Date.now());
+    }
+  }
+
+  // Affichage du popup + de la donnée de l'API via axios :
   const showModal = (id) => {
     setOpenModal(true);
+
+    if(localStorage.getItem(id)){
+      setCharacterInfo(JSON.parse(localStorage.getItem(id)));
+      setLoading(false);
+    } else {
+      axios
+      .get(`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`)
+      .then((response) => {
+        setCharacterInfo(response.data);
+        setLoading(false);
+        localStorage.setItem(id, JSON.stringify(response.data));
+        
+        if(!localStorage.getItem('marvelStorageDate')){
+          localStorage.setItem('marvelStorageDate', Date.now());
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }
   }
 
   // Fermeture du popup :
   const hideModal = () => {
     setOpenModal(false);
+    setLoading(true);
   }
 
   // Note moyenne obtenue aux questions :
@@ -110,6 +155,46 @@ const QuizOver = React.forwardRef((props, ref) => {
     </td>
   </tr>
  )
+
+ // Variable pour afficher les résultats dans la modale après le loading :
+ const resultInModal = !loading ? (
+  <Fragment>
+    <div className='modalHeader'>
+      <h2>{characterInfo.data.results[0].name}</h2>
+    </div>
+    <div className='modalBody'>
+      <div className='comicImage'>
+        <img src={characterInfo.data.results[0].thumbnail.path+'.'+characterInfo.data.results[0].thumbnail.extension} alt="comic character pic"/>
+        <p>{characterInfo.attributionText}</p>
+      </div>
+      <div className='comicDetails'>
+        <h3>Description</h3>
+        {
+          characterInfo.data.results[0].description ? 
+          <p>characterInfo.data.results[0].description</p> : <p>Description indisponible</p>
+        }
+        <h3>Plus d'infos</h3>
+        {
+          characterInfo.data.results[0].urls && characterInfo.data.results[0].urls.map((url, index) => {
+            return <a key={index} href={url.url} target='_blank' rel='noopener noreferrer'>{url.type}</a>
+          })
+        }
+      </div>
+    </div>
+    <div className='modalFooter'>
+      <button className='modalBtn' onClick={hideModal}>Fermer</button>
+    </div>
+  </Fragment>
+ ) : (
+  <Fragment>
+    <div className='modalHeader'>
+      <h2>Résponse de Marvel...</h2>
+    </div>
+    <div className='modalBody'>
+      <Loader />
+    </div>
+  </Fragment>
+ )
  
   return (
     <Fragment>
@@ -134,15 +219,7 @@ const QuizOver = React.forwardRef((props, ref) => {
       </div>
 
       <Modal showModal={openModal} hideModal={hideModal}> 
-        <div className='modalHeader'>
-          <h2>Titre</h2>
-        </div>
-        <div className='modalBody'>
-          <h3>Titre</h3>
-        </div>
-        <div className='modalFooter'>
-          <button className='modalBtn'>Fermer</button>
-        </div>
+        {resultInModal}
       </Modal>
 
     </Fragment>
